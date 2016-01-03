b95ca52027003a8918f4acc05bef003a3baf5f7f
James Lucas <LucasJ94@hotmail.co.uk>
2016-01-03 11:30:26 -0800

    rustfmt

--- src/loader.rs
+++ src/loader.rs
@@ -6,52 +6,61 @@ use std::str::FromStr;
 use datatable::*;
 
 pub struct Loader<'a> {
-	file: &'a str,
-	delimiter: char,
+    pub file: &'a str,
+    pub delimiter: char,
 }
 
 impl<'a> Loader<'a> {
+    /// Load the file from the loader with given delimiter.
+    ///
+    /// Pretty rudimentary with poor error handling.
+    pub fn load_file(self) -> Result<DataTable, io::Error> {
+        let f = try!(File::open(self.file));
+        let reader = BufReader::new(f);
 
-	/// Load the file from the loader with given delimiter.
-	///
-	/// Pretty rudimentary with poor error handling.
-	pub fn load_file(self) -> Result<DataTable, io::Error> {
-		let f = try!(File::open(self.file));
-		let reader = BufReader::new(f);
+        let mut table = DataTable::empty();
 
-		let mut table = DataTable::empty();
+        // Should handle header here
 
-		// Should handle header here
+        let mut lines = reader.lines();
 
-		let mut lines = reader.lines();
+        if let Some(line) = lines.next() {
+            let line = try!(line);
+            let values = line.split(self.delimiter);
 
-		if let Some(line) = lines.next() {
-			let line = try!(line);
-			let values = line.split(self.delimiter);
+            for val in values {
+                let mut column = DataCols::<f64>::empty();
+                // Unwrap and panic for now.
+                // Should handle more carefully in future.
+                column.data.push(f64::from_str(val).unwrap());
 
-			for val in values {
-				let mut column = DataCols::<f64>::empty();
-				// Unwrap and panic for now.
-				// Should handle more carefully in future.
-				column.data.push(f64::from_str(val).unwrap());
+                table.data_cols.push(column);
+            }
+        }
 
-				table.data_cols.push(column);
-			}
-		}
+        for line in lines {
+            let line = try!(line);
+            let values = line.split(self.delimiter);
 
-		for line in lines {
-		    let line = try!(line);
-		    let values = line.split(self.delimiter);
+            for (i, val) in values.enumerate() {
 
-		    for (i,val) in values.enumerate() {
+                // Unwrap and panic for now.
+                // Should handle more carefully in future.
+                let val = f64::from_str(val).unwrap();
+                table.data_cols[i].data.push(val);
+            }
+        }
 
-				// Unwrap and panic for now.
-				// Should handle more carefully in future.
-				let val = f64::from_str(val).unwrap();
-				table.data_cols[i].data.push(val);
-			}
-		}
+        Ok(table)
+    }
+}
 
-		Ok(table)
-	}
+pub fn load_file(file: &str) -> DataTable {
+    let loader = Loader {
+        file: file,
+        delimiter: ',',
+    };
+
+    loader.load_file().unwrap()
+
 }
